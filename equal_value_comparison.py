1a99524537bf72ffef65d8159b675c2487d47f8b
wahyuadjiep <wahyuadjieprasetya1@gmail.com>
2021-12-19 15:14:53 +0700

    perbandingan senilai  final

---
+++ equal_value_comparison.py
@@ -0,0 +1,15 @@
+
+y1 = int(input("enter Y1 = "))
+y2 = int(input("enter y2 = "))
+x1 = int(input("enter X1 = "))
+
+result_value_y = y2 * x1
+result_value_x = y1
+
+result = result_value_y / result_value_x
+
+print(result_value_y)
+print("==============")
+print(result_value_x)
+
+print("result X2 : ", result)
